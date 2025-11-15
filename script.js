5669dfcfed80e83425d40b11ba17793ffea171f9
quang <quangb2016996@student.ctu.edu.vn>
2025-11-15 11:17:36 +0700

    Khung mẫu

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Trang chủ
 
+// San pham
+// - Tạo chức năng thêm sản phẩm vào giỏ hàng, kiểm tra bằng localStorage.getItem()
+
+// Dang nhap
+// - Đăng nhập bằng thông tin lưu trữ ở local storage
+
+// Dang ky
+// - Tạo chức năng đăng ký, lưu trữ thông tin đăng nhập ở local storage (key là "name" và "psw"), kiểm tra bằng localStorage.getItem()
+
+// Lien he
+
+// Gioi thieu
+// - Tạo slide show sản phẩm
+// https://www.w3schools.com/howto/howto_js_slideshow.asp
+
+// Gio hang
